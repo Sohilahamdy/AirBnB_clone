0b8948bd0840ea6a0b1e43527f6dbf5766dcf1df
Sohila hamdy <sohila.fathy22@gmail.com>
2024-07-22 13:19:57 -0700

    0x00. AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -16,14 +16,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         model_dict = self.__dict__.copy()
