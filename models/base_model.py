4b7764825d3c9f39825a704853fad6391c43565a
Sohila hamdy <sohila.fathy22@gmail.com>
2024-07-22 06:28:36 -0700

    0x00. AirBnB clone - The console

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    """A base class for all models in the application."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -20,15 +17,12 @@ class BaseModel:
             self.updated_at = datetime.utcnow()
 
     def __str__(self):
-        """Return a string representation of the instance."""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
-        """Return a dictionary representation of the instance."""
         dict_representation = self.__dict__.copy()
         dict_representation["__class__"] = self.__class__.__name__
         dict_representation["created_at"] = self.created_at.isoformat()
