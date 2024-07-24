71bd73867c3c3a07276fa4854e6668feeafd00c0
Sohila hamdy <sohila.fathy22@gmail.com>
2024-07-24 09:51:31 -0700

    0x00. AirBnB clone - The console

--- console.py
+++ console.py
@@ -2,37 +2,25 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for the AirBnB project.
+    """Command interpreter for the AirBnB project."""
     
-    This class handles the command line interface for managing 
-    AirBnB objects. It supports commands like quit, EOF, and help.
-    """
+    prompt = "(hbnb) "
     
-    prompt = '(hbnb) '
-
     def do_quit(self, line):
-        """Quit command to exit the program.
-
-        Usage: quit
-        """
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, line):
-        """Exit the program when EOF (Ctrl+D) is encountered.
-
-        Usage: EOF
-        """
+        """Exit the program when EOF (Ctrl+D) is encountered."""
+        print("")
         return True
 
     def do_help(self, line):
-        """Display help information about commands.
-
-        Usage: help [command]
-        """
+        """Display help information about commands."""
         super().do_help(line)
 
     def emptyline(self):
-        """Override the emptyline method to do nothing on an empty input line."""
+        """Do nothing upon receiving an empty line."""
         pass
 
 if __name__ == '__main__':
