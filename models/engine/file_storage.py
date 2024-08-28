6e943388329856056af166bf5f2e7bf89e114012
Sohilahamdy <128439046+Sohilahamdy@users.noreply.github.com>
2024-08-28 22:00:45 +0300

    update errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,28 +30,18 @@ class FileStorage():
 
     def save(self):
         """Save method documentation"""
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, mode='w') as my_file:
-            json.dump(new_dict, my_file)
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump({key: obj.to_dict() for key, obj in FileStorage.__objects.items()}, file)
 
     def reload(self):
         """Reloads the stored objects from the file"""
-        if not os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'w') as file:
-                file.write('{}')  # Create an empty JSON file
-
         try:
-            with open(FileStorage.__file_path, mode='r') as my_file:
-                new_dict = json.load(my_file)
-
-            for key, value in new_dict.items():
-                class_name = value.get('__class__')
-                obj = eval(class_name + '(**value)')
-                FileStorage.__objects[key] = obj
+            with open(FileStorage.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name = value['__class__']
+                    cls = globals()[class_name]
+                    FileStorage.__objects[key] = cls(**value)
 
         except FileNotFoundError:
-            pass
-        except JSONDecodeError:
             pass
